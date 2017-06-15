24cfae4715c7cbcd13055819a8d3f1cfc8412157
Benjamin Schneider <Benjamin.Schneider@hmmh.de>
2017-06-15 09:44:45 +0200

    added BaseDialogFragment & fade_in, fade_out animations

---
+++ app/src/main/java/de/skicomp/dialogs/BaseDialogFragment.java
@@ -0,0 +1,10 @@
+package de.skicomp.dialogs;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by benjamin.schneider on 14.06.17.
+ */
+
+public class BaseDialogFragment extends DialogFragment {
+}
